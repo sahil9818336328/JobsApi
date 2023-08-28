1d563c1e922634fb0f5fc3792953bf4b6bbdac6a
Sahil khatri <Sahilkehsav1997@gmail.com>
2023-08-28 16:49:38 +0530

    added security packages

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
-const app = express()
 
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
+const app = express()
 const connectDB = require('./db/connect')
 
 const authRouter = require('./routes/auth')
@@ -13,8 +18,16 @@ const authenticateUser = require('./middleware/authentication')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+)
 app.use(express.json())
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
